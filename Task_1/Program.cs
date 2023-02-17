7a3a71ce10bf722eb2e7db7fe1e2a5a2923c52a2
Mikhail Kozlov <kozlov_m_v@mail.ru>
2023-02-17 15:09:27 +0300

    add task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿string inputValue;
+
+Console.Write("Введите 1-е число: ");
+inputValue = Console.ReadLine();
+int number_1 = Convert.ToInt32(inputValue);
+
+Console.Write("Введите 2-е число: ");
+inputValue = Console.ReadLine();
+int number_2 = Convert.ToInt32(inputValue);
+
+if (number_1 == number_2)
+{
+    System.Console.WriteLine("Числа равны");
+}
+else
+{
+    if (number_1 < number_2)
+    {
+        System.Console.WriteLine($"Число {number_2} больше числа {number_1}");
+    }
+    else
+    {
+        System.Console.WriteLine($"Число {number_1} больше числа {number_2}");
+    }
+}
+
+
