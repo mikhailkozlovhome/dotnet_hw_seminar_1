e74a72d3ec59dc4589df8cf510a0f2a78ba53b62
Mikhail Kozlov <kozlov_m_v@mail.ru>
2023-02-17 15:24:26 +0300

    add task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿string inputValue;
+
+Console.Write("Введите число: ");
+inputValue = Console.ReadLine();
+int number = Convert.ToInt32(inputValue);
+
+if (number % 2 == 0) 
+{
+    System.Console.WriteLine($"Число {number} четное.");
+}
+else
+{
+    System.Console.WriteLine($"Число {number} нечетное.");
+}
