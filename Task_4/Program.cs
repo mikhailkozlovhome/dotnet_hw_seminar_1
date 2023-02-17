bee385bcf4c9d56cec3d446f1bc295ce08e8a9b6
Mikhail Kozlov <kozlov_m_v@mail.ru>
2023-02-17 15:29:59 +0300

    add task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿string inputValue;
+int count = 2;
+
+Console.Write("Введите число: ");
+inputValue = Console.ReadLine();
+int number = Convert.ToInt32(inputValue);
+
+while (count <= number)
+{
+    Console.Write(count + " ");
+    count = count + 2;
+}
+
