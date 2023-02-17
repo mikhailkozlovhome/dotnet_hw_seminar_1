802f032b5f664a72ee3c66f3d497cdd9f4a50fa4
Mikhail Kozlov <kozlov_m_v@mail.ru>
2023-02-17 15:18:41 +0300

    add task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿string inputValue;
+
+Console.Write("Введите 1-е число: ");
+inputValue = Console.ReadLine();
+int number_1 = Convert.ToInt32(inputValue);
+
+Console.Write("Введите 2-е число: ");
+inputValue = Console.ReadLine();
+int number_2 = Convert.ToInt32(inputValue);
+
+Console.Write("Введите 3-е число: ");
+inputValue = Console.ReadLine();
+int number_3 = Convert.ToInt32(inputValue);
+
+int max = number_1;
+
+if (number_2 > max)
+{
+    max = number_2;
+}
+
+if (number_3 > max)
+{
+    max = number_3;
+}
+
+System.Console.WriteLine("Максимальное число равно: " + max);
